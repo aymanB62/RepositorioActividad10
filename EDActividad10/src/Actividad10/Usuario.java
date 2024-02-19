bc55beae60933500a29ac3cd02898874f838dc23
Ayman <oumgharayman2004@gmail.com>
2024-02-19 13:18:09 +0100

    segundo commit, creacion Usuario

---
+++ EDActividad10/src/Actividad10/Usuario.java
@@ -0,0 +1,49 @@
+package Actividad10;
+
+import java.util.Arrays;
+
+public class Usuario {
+	
+	String id;
+	String nombre;
+	double[] valoraciones;
+
+	public Usuario() {
+		
+	}
+	
+	public Usuario(String id, String nombre, double[] array) {
+		this.id = id;
+		this.nombre = nombre;
+		this.valoraciones = array;
+	}
+
+	public String toString() {
+		return "Usuario [id = " + id + ", nombre = " + nombre + ", valoraciones = " + Arrays.toString(valoraciones) + "]";
+	}
+	
+	public int mediaDeValoraciones() {
+		int sumador = 0;
+		for(int i = 0; i < this.valoraciones.length; i++) {
+			sumador += this.valoraciones[i];
+		}
+		int media = sumador / this.valoraciones.length;
+		return media;
+	}
+	
+	public void imprimirValoraciones() {
+		System.out.println(Arrays.toString(valoraciones));
+	}
+	
+	public void valoracionMayorMenor(Usuario v) {
+		int sumador = 0;
+		for(int i = 0; i < this.valoraciones.length; i ++) {
+			v.valoraciones[1] = this.valoraciones[i];
+				if(this.valoraciones[i] > v.valoraciones[1]) {
+					sumador++;
+				}
+		}
+		System.out.println("Hay  " + sumador + "mayor");
+	}
+	
+}
