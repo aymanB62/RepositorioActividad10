f6e1b24d42176ea9e9fd7b3f774bfe42e6962334
alejandro <grajalesalejandro068@gmail.com>
2024-02-20 12:13:17 +0100

    creacion de la clase main alejandro

---
+++ EDActividad10/src/Actividad10/Main.java
@@ -0,0 +1,40 @@
+package Actividad10;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Usuario u1 = new Usuario("208934H" , "HOLA", pedirPorPantalla());
+
+		System.out.println(u1.toString());
+		
+		System.out.println("La media de valoraciones es: " + u1.mediaDeValoraciones());
+		
+		u1.imprimirValoraciones();
+		
+		u1.valoracionMayorMenor(u1);
+		
+		
+	}
+	
+	public static double[] pedirPorPantalla() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Cuantas valoraciones vas ha introducir?");
+		int num1 = sc.nextInt();
+		
+		double[] valoraciones = new double[num1];
+		
+		for(int i = 0; i < num1; i++) {
+			System.out.println("Introduce un numero: ");
+			valoraciones[i] = sc.nextDouble();
+		}
+		return valoraciones;
+	}
+
+	
+}
+
